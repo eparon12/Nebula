520f91c43777099a97b0ed19d6f44bb379bbb207
eparon12 <eparon.mc@gmail.com>
2025-11-18 08:21:22 -0500

    finished with looping method adjusted to returning a blockpos

--- src/main/java/com/eparon/nebula/macros/mineWoodMacro.java
+++ src/main/java/com/eparon/nebula/macros/mineWoodMacro.java
@@ -8,6 +8,7 @@ import net.minecraft.scoreboard.ScoreObjective;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import org.lwjgl.input.Mouse;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -15,81 +16,107 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.block.Block;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldServer;
 // 8,
 public class mineWoodMacro {
     final EntityPlayerSP player = mc.thePlayer;
+    MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
+    WorldServer world = server.worldServers[0];
+
     boolean targetFound = false;
-    int searchedBlocksCounter = 0;
-    int searchedBlocksCounterIncrement = 1;
-    int searchedBlocksCounterTarget = 1 ;
-    int searchedBlocksRing = 1;
     public static void WoodMacroMain() {
         final Minecraft mc = Minecraft.getMinecraft();
+        BlockPos targetBlockToBreak = new BlockPos();
+        BlockPos targetBlockToBreak = loop(player.posx, player.posy, player.posz, "minecraft:cobblestone");
         do{
-            //int seatchedBlocksCounterTarget = Math.pow(searchedBlocksCounterIncrement, 2) - Math.pow(searchedBlocksCounterIncrement - 2, 2);
-            for(int i = 1; i <= searchedBlocksCounterTarget; i++) {
-                for(int j = 0; j <  searchedBlocksRing * 2 + 1; j++) {
-                    BlockPos _pos = new BlockPos(player.posx - searchedBlocksRing + j, player.posy, player.posz - searchedBlocksRing);
-                }
-                //BlockPos pos = new BlockPos(player.posx, player.posy, player.posz - searchedBlocksRing);
 
-
-
-            }
 
         } while(targetFound == false);
         KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), true);
-        }
+
     }
-    public static int[] loop(int px, int py, int pz, String targetBlock){
-        int[] coords = {px, py, pz};
+    public static BlockPos loop(double px, double py, double pz, String targetBlockName){
+        Block targetBlock = new Block();
+        targetBlock = Block.getBlockFromName(targetBlockName);
+        double[] coords = {px, py, pz};
         int searchedRings = 0;
-        while(searchedRings != 3){
+
+        while(searchedRings <= 128){
 
             searchedRings++;
-            System.out.println("--------Starting-------\n"+ " Rings to search: " + searchedRings);
+            player.addChatMessage(new ChatComponentText("Starting -- Rings to search: " + searchedRings));
             int searched = 0;
             double searchedTarget = Math.pow(2 * (searchedRings + 1) - 1, 2) - Math.pow(2 * (searchedRings) - 1, 2);
             int x = px - searchedRings;
             int z = pz - searchedRings;
             coords[0] = x;
             coords[2] = z;
-            System.out.println("Blocks to be searched: " + searchedTarget);
-            System.out.println(coords[0] + " " +  coords[1] + " " +  coords[2]);
+            player.addChatMessage(new ChatComponentText("Blocks to be searched: " + searchedTarget));
+            player.addChatMessage(new ChatComponentText(coords[0] + " " +  coords[1] + " " +  coords[2]));
 
             for(int i = 0; i < searchedRings * 2; i++){
                 searched++;
                 x++;
                 coords[0] = x;
                 coords[2] = z;
-                System.out.println(coords[0] + " " +  coords[1] + " " +  coords[2]);
+                player.addChatMessage(new ChatComponentText(coords[0] + " " +  coords[1] + " " +  coords[2]));
+                BlockPos _pos = new BlockPos(coords[0], coords[1], coords[2]);
+                IBlockState blockState = world.getBlockState(_pos);
+                Block _block = blockState.getBlock();
+                if(_block == targetBlock) {
+                    return _pos;
+                }
             }
-            System.out.println(coords[0] + " " +  coords[1] + " " +  coords[2]);
+            player.addChatMessage(new ChatComponentText(coords[0] + " " +  coords[1] + " " +  coords[2]));
             for(int i = 0; i < searchedRings * 2; i++){
                 searched++;
                 z++;
                 coords[0] = x;
                 coords[2] = z;
-                System.out.println(coords[0] + " " +  coords[1] + " " +  coords[2]);
+                player.addChatMessage(new ChatComponentText(coords[0] + " " +  coords[1] + " " +  coords[2]));
+                BlockPos _pos = new BlockPos(coords[0], coords[1], coords[2]);
+                IBlockState blockState = world.getBlockState(_pos);
+                Block _block = blockState.getBlock();
+                if(_block == targetBlock) {
+                    return _pos;
+                }
             }
             for(int i = 0; i < searchedRings * 2; i++){
                 searched++;
                 x--;
                 coords[0] = x;
                 coords[2] = z;
-                System.out.println(coords[0] + " " +  coords[1] + " " +  coords[2]);
+                player.addChatMessage(new ChatComponentText(coords[0] + " " +  coords[1] + " " +  coords[2]));
+                BlockPos _pos = new BlockPos(coords[0], coords[1], coords[2]);
+                IBlockState blockState = world.getBlockState(_pos);
+                Block _block = blockState.getBlock();
+                if(_block == targetBlock) {
+                    return _pos;
+                }
             }
             for(int i = 0; i < searchedRings * 2; i++){
                 searched++;
                 z--;
                 coords[0] = x;
                 coords[2] = z;
-                System.out.println(coords[0] + " " +  coords[1] + " " +  coords[2]);
+                player.addChatMessage(new ChatComponentText(coords[0] + " " +  coords[1] + " " +  coords[2]));
+                BlockPos _pos = new BlockPos(coords[0], coords[1], coords[2]);
+                IBlockState blockState = world.getBlockState(_pos);
+                Block _block = blockState.getBlock();
+                if(_block == targetBlock) {
+                    return _pos;
+                }
 
             }
-            System.out.println("-------------Done-------------");
+            player.addChatMessage(new ChatComponentText(("---------------Done-------------"));
         }
+        BlockPos notFound = new BlockPos(px, py, pz);
+        player.addChatMessage(new ChatComponentText("Block not found, returning player's position"));
+        return notFound;
 
-        return coords;
+
+
     }
 }
